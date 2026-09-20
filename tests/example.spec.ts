d05799e34232be9134a5ab2536ff46de4e471a91
Phong Do <dominhphong306@gmail.com>
2026-09-20 21:08:51 +0700

    QA-4: delete unuse tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-  await expect(page).toHaveTitle(/Fast and reliable/);
-});
-
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
