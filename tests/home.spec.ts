caf519d0266c98aa2220ed28c345815073f2a8b6
Phong Do <dominhphong306@gmail.com>
2026-09-20 21:16:54 +0700

    QA-4: remove redundant line

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('Home page - expect title display', async ({ page }) => {
   await page.goto('https://e-commerce-dev.betterbytesvn.com/');
-
   // Expect a title "to contain" a substring.
   await expect(page.getByRole("heading", { name: "E-commerce site testing"})).toBeVisible();
 });
